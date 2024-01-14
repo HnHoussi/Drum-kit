6c6b7fde04f244fb3526fca55cf2b83d26689c90
Houcine Hannoun <hanoun.houssin@gmail.com>
2024-01-14 15:53:01 +0100

    Refactor the code

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
 
-const clapAudio = document.getElementById("clap-audio")
-const hihatAudio = document.getElementById("hihat-audio")
-const kickAudio = document.getElementById("kick-audio")
-const openhatAudio = document.getElementById("openhat-audio")
-const boomAudio = document.getElementById("boom-audio")
-const rideAudio = document.getElementById("ride-audio")
-const snareAudio = document.getElementById("snare-audio")
-const tomAudio = document.getElementById("tom-audio")
-const tinkAudio = document.getElementById("tink-audio")
+function animationHandling(button) {
+    button.classList.add('clicked', 'animated');
+    setTimeout(function(){
+        button.classList.remove('clicked', 'animated')
+    }, 150);
+}
 
-document.addEventListener('keydown', function(event) {
+function audioHandling(audioId) {
+    const audio = document.getElementById(audioId);
+    audio.currentTime = 0;
+    audio.play()
+}
 
-    if (event.key.toUpperCase() === 'Q') {
-        if(clapAudio.paused){
-            clapAudio.play()
-        } else {
-            clapAudio.pause();
-            clapAudio.currentTime = 0;
-        }
+function keyDown(event) {
+    var clickedButton = document.querySelector('.child-btn[data-key="' + event.key.toUpperCase() + '"]');
+
+    if (clickedButton) {
+        animationHandling(clickedButton);
+        audioHandling('audio' + event.key.toUpperCase())
     }
-    if (event.key.toUpperCase() === 'S') {
-        hihatAudio.play()
-    }
-    if (event.key.toUpperCase() === 'D') {
-        kickAudio.play()
-    }
-    if (event.key.toUpperCase() === 'F') {
-        openhatAudio.play()
-    }
-    if (event.key.toUpperCase() === 'G') {
-        boomAudio.play()
-    }
-    if (event.key.toUpperCase() === 'H') {
-        clapAudio.play()
-    }
-    if (event.key.toUpperCase() === 'J') {
-        snareAudio.play()
-    }
-    if (event.key.toUpperCase() === 'K') {
-        tomAudio.play()
-    }
-    if (event.key.toUpperCase() === 'L') {
-        tinkAudio.play()
-    }
-  });
+}
+
+document.addEventListener('keydown', keyDown)
